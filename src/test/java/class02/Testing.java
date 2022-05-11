b78a50a5b866e9379c8f91418d1167276928d96d
Volkan20 <volkandereli1@gmail.com>
2022-05-10 22:16:37 -0400

    class02 TestNg

---
+++ src/test/java/class02/Testing.java
@@ -0,0 +1,53 @@
+package class02;
+
+import org.testng.annotations.*;
+
+public class Testing {
+
+    @BeforeSuite
+    public void beforeSuit(){
+        System.out.println("BeforeSuitttt");
+    }
+    @AfterSuite
+    public void afterSuit(){
+        System.out.println("AfterSuittttt");
+    }
+
+
+ @BeforeTest
+ public void beforeTest(){
+     System.out.println("Before Test :)");
+ }
+ @AfterTest
+ public void afterTest(){
+     System.out.println("After Test :)");
+ }
+
+
+@BeforeClass
+public void beforeClass(){
+    System.out.println("I am before class method");
+}
+@AfterClass
+public void afterClass(){
+    System.out.println("I am after class method");
+}
+
+    @BeforeMethod
+    public void before(){
+        System.out.println("This is my before method");
+    }
+    @AfterMethod
+    public void after(){
+        System.out.println("This is my after method");
+    }
+
+    @Test
+    public void Test(){
+        System.out.println("driver me crazy, drive me mad");
+    }
+    @Test
+    public void Test2(){
+        System.out.println("driver me to sanity, driver me to hell");
+    }
+}
---
+++ src/test/java/class02/Testing2.java
@@ -0,0 +1,14 @@
+package class02;
+
+import org.testng.annotations.Test;
+
+public class Testing2 {
+    @Test
+    public void Testa(){
+        System.out.println("driver me bikeeee, drive me bikjeeee");
+    }
+    @Test
+    public void Testb(){
+        System.out.println("driver me 9999999, drive me 99999");
+    }
+}
